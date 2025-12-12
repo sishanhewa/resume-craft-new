5abdd553e4efab95eacfda2963ba7be123c584b0
Sishanhewapathirana <sishanhewa4@gmail.com>
2025-12-13 01:02:07 +0530

    fix build errors

--- src/app/auth/signout/route.ts
+++ src/app/auth/signout/route.ts
@@ -2,6 +2,8 @@ import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 import { type NextRequest, NextResponse } from "next/server";
 
+export const runtime = 'edge';
+
 export async function POST(req: NextRequest) {
     const cookieStore = await cookies();
 
